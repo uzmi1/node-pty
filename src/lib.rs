9c16d8a483ac2c9ff1c202e8ba4e0d099c84764c
Daniel Brenot <daniel.brenot@canarie.ca>
2021-11-02 19:27:38 -0400

    Batch of changes to fix compile errors

--- src/lib.rs
+++ src/lib.rs
@@ -6,35 +6,38 @@ extern crate napi_derive;
 extern crate serde_derive;
 //#[macro_use]
 //extern crate napi_derive;
-
-use std::fmt::Result;
 use std::fs;
+use std::os::unix::prelude::AsRawFd;
+use std::os::unix::prelude::RawFd;
 use std::ptr::null;
 use std::ffi::c_void;
-
+use std::convert::TryFrom;
 
 
 // import the preludes
 use napi::bindgen_prelude::*;
+use napi::Result;
 
-use nix::fcntl::FcntlArg::{F_GETFL, F_SETFL};
 
-use nix::libc::tcgetattr;
+use nix::libc::*;
 
+use nix::pty::PtyMaster;
+use nix::pty::openpty;
+use nix::sys::termios::Termios;
 use nix::unistd::execvp;
 use nix::libc::{O_NONBLOCK, TIOCSWINSZ, CTL_KERN, KERN_PROC, KERN_PROC_PID, winsize};
+use nix::libc::{EBADF, EFAULT, EINVAL, ENOTTY};
 use nix::unistd::tcgetpgrp;
 use nix::pty::ptsname;
 use nix::errno::errno;
-use nix::errno::Errno::{EBADF, EFAULT, EINVAL, ENOTTY};
 
-use nix::libc::{ioctl, sysctl, FILE };
-use nix::fcntl::{fcntl};
+use nix::libc::{ioctl, sysctl, FILE, fcntl, termios};
 
 use std::ffi::CStr;
 
 use serde_json::{Map, Value};
 use serde::{Deserialize, Serialize};
+
 use napi_derive::napi;
 
 
@@ -53,7 +56,7 @@ struct IUnixProcess {
     pub pty: String
 }
 
-//#[napi(object)]
+#[napi(object)]
 #[derive(Serialize, Deserialize, Debug)]
 struct IUnixOpenProcess {
     pub master: i32,
@@ -63,21 +66,25 @@ struct IUnixOpenProcess {
 
 // Exposed functions for NAPI
 
-/// Fork
+
 #[napi]
 fn fork<T: Fn(i32,i32) -> Result<()>>(
     file: String, args: Vec<String>,
     env: Vec<String>, cwd: String,
     cols: i32, rows: i32,
     uid: i32, gid: i32,
-    utf8: bool, onexit: T) -> Result<IUnixProcess> {
+    utf8: bool, onexit: T) -> napi::Result<IUnixProcess> {
 
-    let mut term = termios::;
-    term.c_iflag = ICRNL | IXON | IXANY | IMAXBEL | BRKINT;
-    term.c_oflag = OPOST | ONLCR;
-    term.c_cflag = CREAD | CS8 | HUPCL;
-    term.c_lflag = ICANON | ISIG | IEXTEN | ECHO | ECHOE | ECHOK | ECHOKE | ECHOCTL;
-    
+    let term = termios {
+        c_iflag: ICRNL | IXON | IXANY | IMAXBEL | BRKINT,
+        c_oflag: OPOST | ONLCR,
+        c_cflag: CREAD | CS8 | HUPCL,
+        c_lflag: ICANON | ISIG | IEXTEN | ECHO | ECHOE | ECHOK | ECHOKE | ECHOCTL,
+        c_cc: val,
+        c_ispeed: val,
+        c_ospeed: val
+    };
+
     if utf8 { term.c_iflag |= IUTF8; }
 
     term.c_cc[VEOF] = 4;
@@ -108,7 +115,7 @@ fn fork<T: Fn(i32,i32) -> Result<()>>(
 
     let fd = -1;
     let newmask;
-    let oldmask; 
+    let oldmask;
 
 
     // TODO fill in remaining implementation
@@ -117,48 +124,62 @@ fn fork<T: Fn(i32,i32) -> Result<()>>(
 
 }
 
-//#[napi]
-fn open(cols: u16, rows: u16) -> Result<IUnixOpenProcess> {
+#[napi]
+fn open(cols: u32, rows: u32) -> napi::Result<IUnixOpenProcess> {
+    let ws_col = u16::try_from(cols);
+    let ws_row = u16::try_from(rows);
+    if ws_col.is_err() { return Err(napi::Error::new(napi::Status::InvalidArg, String::from("Failed to convert cols to a u16"))) }
+    if ws_row.is_err() { return Err(napi::Error::new(napi::Status::InvalidArg, String::from("Failed to convert rows to a u16"))) }
     let winp = winsize {
-        ws_col: cols, ws_row: rows,
+        ws_col: ws_col.unwrap(), ws_row: ws_row.unwrap(),
         ws_xpixel: 0, ws_ypixel: 0
     };
     let (master, slave) = pty_openpty(winp, None, None)?;
 
-    if pty_nonblock(master) == -1 { Err("Could not set master fd to nonblocking.") }
-    if pty_nonblock(slave) == -1 { Err("Could not set slave fd to nonblocking.") }
-    
+    pty_nonblock(master.as_raw_fd())?;
+    pty_nonblock(slave.as_raw_fd())?;
+
     // Takes the result from ptsname and converts it to a string for easy serialization
-    let pty = unsafe { ptsname(master)? };
-    return Ok(IUnixOpenProcess {master, slave, pty});
+    let pty = unsafe { ptsname(&master) };
+    if pty.is_err() { return Err(napi::Error::new(napi::Status::GenericFailure, "Failed to get the name of pty".to_string())); }
+    return Ok(IUnixOpenProcess {master: master.as_raw_fd(), slave: slave.as_raw_fd(), pty: pty.unwrap()});
 }
 
-//#[napi]
+#[napi]
 fn process(fd: i32, tty: String) -> Option<String> {
     // TODO do we want to replace this with a result and throw an error instead?
     if tty.is_empty() {return None; }
     let name = pty_getproc(fd, tty);
     match name {
-        Ok(name) => todo!(),
+        Ok(name) => return Some(name),
         Err(err) => return None,
     }
 }
 
-//#[napi]
+#[napi]
 fn resize(fd: i32, cols: i32, rows: i32) -> Result<()>{
+    let ws_col = u16::try_from(cols);
+    let ws_row = u16::try_from(rows);
+    if ws_col.is_err() { return Err(napi::Error::new(napi::Status::InvalidArg, String::from("Failed to convert cols to a u16"))) }
+    if ws_row.is_err() { return Err(napi::Error::new(napi::Status::InvalidArg, String::from("Failed to convert rows to a u16"))) }
     let winp = winsize {
-        ws_col: cols, ws_row: rows,
+        ws_col: ws_col.unwrap(), ws_row: ws_row.unwrap(),
         ws_xpixel: 0, ws_ypixel: 0
     };
     if (unsafe { ioctl(fd, TIOCSWINSZ, &winp) } == -1) {
-        match errno() {
-            EBADF => Err("ioctl(2) failed, EBADF"),
-            EFAULT => Err("ioctl(2) failed, EFAULT"),
-            EINVAL => Err("ioctl(2) failed, EINVAL"),
-            ENOTTY => Err("ioctl(2) failed, ENOTTY"),
-            _ => Err("ioctl(2) failed")
-        }
+        Err(napi::Error::new(napi::Status::GenericFailure,
+    String::from(match errno() {
+                EBADF => "ioctl(2) failed, EBADF",
+                EFAULT => "ioctl(2) failed, EFAULT",
+                EINVAL => "ioctl(2) failed, EINVAL",
+                ENOTTY => "ioctl(2) failed, ENOTTY",
+                _ => "ioctl(2) failed"
+            })
+        ))
+    } else {
+        Ok(())
     }
+
 }
 
 // Helper functions to be used internally
@@ -167,13 +188,15 @@ fn resize(fd: i32, cols: i32, rows: i32) -> Result<()>{
 /// http://www.gnu.org/software/gnulib/manual/html_node/execvpe.html
 fn pty_execvpe(file: String, argv: *const *const i8, envp: *const *const i8) -> i32 {
     // TODO implementation here
-}   
+}
 
 /// Nonblocking FD
-fn pty_nonblock(fd: i32) -> Result<()> {
-    let flags = fcntl(fd, F_GETFL, 0)?;
-    if flags == -1 { return -1; }
-    return fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+fn pty_nonblock(fd: RawFd) -> Result<i32> {
+    let flags = unsafe { fcntl(fd, F_GETFL, 0) };
+    if flags == -1 { return Err(napi::Error::new(napi::Status::GenericFailure, "Failed at fcntl F_GETFL".to_string())); }
+    flags = unsafe { fcntl(fd, F_SETFL, flags | O_NONBLOCK) };
+    if flags == -1 { return Err(napi::Error::new(napi::Status::GenericFailure, "Failed at fcntl F_SETFL".to_string())); }
+    Ok(flags)
 }
 
 /// pty_waitpid
@@ -195,8 +218,7 @@ fn pty_after_close() {
 }
 
 /// Taken from: tmux (http://tmux.sourceforge.net/)
-#[cfg(not(target_os = "macos"))]
-#[cfg(not(target_os = "linux"))]
+#[cfg(not(any(target_os = "macos", target_os = "linux")))]
 fn pty_getproc(fd: i32, tty: String) -> Result<String> {
     return Err(());
 }
@@ -230,7 +252,7 @@ fn pty_getproc(fd: i32, tty: String) -> Result<String> {
 }
 
 /// Returns the master and slave fd's in a tuple
-fn pty_openpty(winp: winsize, name: Option<String>, termp: Option<&Termios>) -> Result<(i32, i32)> {
+fn pty_openpty(winp: winsize, name: Option<String>, termp: Option<&Termios>) -> Result<(PtyMaster, PtyMaster)> {
     // TODO implementation here
     return Ok((0,0));
 }

f4882df1e4289ae1f956f54aaf65c4ea1842c37c
Daniel Brenot <daniel.brenot@canarie.ca>
2021-11-03 02:20:52 -0400

    Added documentation and code fixes

--- src/lib.rs
+++ src/lib.rs
@@ -1,44 +1,57 @@
+///
+/// Copyright (c) 2012-2015, Christopher Jeffrey (MIT License)
+/// Copyright (c) 2017, Daniel Imms (MIT License)
+///
+/// pty.rs:
+///  This file is responsible for starting processes
+///  with pseudo-terminal file descriptors.
+///
+/// See:
+///  man pty
+///  man tty_ioctl
+///  man termios
+///  man forkpty
+///
+
+// macros
 #[macro_use]
 extern crate napi;
 #[macro_use]
 extern crate napi_derive;
 #[macro_use]
 extern crate serde_derive;
-//#[macro_use]
-//extern crate napi_derive;
-use std::fs;
+
+// std imports
+use std::borrow::Borrow;
+use std::borrow::BorrowMut;
+use std::ffi::CString;
 use std::os::unix::prelude::AsRawFd;
 use std::os::unix::prelude::RawFd;
 use std::ptr::null;
-use std::ffi::c_void;
 use std::convert::TryFrom;
-
-
-// import the preludes
 use napi::bindgen_prelude::*;
 use napi::Result;
 
-
-use nix::libc::*;
-
+// nix imports
+use nix::env;
 use nix::pty::PtyMaster;
-use nix::pty::openpty;
+use nix::sys::signal::NSIG;
 use nix::sys::termios::Termios;
-use nix::unistd::execvp;
-use nix::libc::{O_NONBLOCK, TIOCSWINSZ, CTL_KERN, KERN_PROC, KERN_PROC_PID, winsize};
-use nix::libc::{EBADF, EFAULT, EINVAL, ENOTTY};
-use nix::unistd::tcgetpgrp;
 use nix::pty::ptsname;
 use nix::errno::errno;
+use nix::unistd::chdir;
 
-use nix::libc::{ioctl, sysctl, FILE, fcntl, termios};
+// nix libc
+use nix::libc::{O_NONBLOCK, TIOCSWINSZ, CTL_KERN, KERN_PROC, KERN_PROC_PID, winsize};
+use nix::libc::{B38400};
+use nix::libc::{EBADF, EFAULT, EINVAL, ENOTTY};
+use nix::libc::{sigfillset, ioctl, sysctl, forkpty, fcntl, termios};
+use nix::libc::{cfsetispeed, cfsetospeed};
+use nix::libc::*;
 
-use std::ffi::CStr;
-
+// Serde
 use serde_json::{Map, Value};
 use serde::{Deserialize, Serialize};
-
-use napi_derive::napi;
 
 
 // use nix::libc::{execvp, ioctl, ptsname, winsize};
@@ -46,7 +59,7 @@ use napi_derive::napi;
 mod conpty_console_list;
 
 
-// Structs
+// Structs for returned data
 
 #[napi(object)]
 #[derive(Serialize, Deserialize, Debug)]
@@ -66,7 +79,7 @@ struct IUnixOpenProcess {
 
 // Exposed functions for NAPI
 
-
+/// Creates a forked process to run the requested file.
 #[napi]
 fn fork<T: Fn(i32,i32) -> Result<()>>(
     file: String, args: Vec<String>,
@@ -75,21 +88,40 @@ fn fork<T: Fn(i32,i32) -> Result<()>>(
     uid: i32, gid: i32,
     utf8: bool, onexit: T) -> napi::Result<IUnixProcess> {
 
+    // fd of the new forked process
+    let mut master = -1;
+    //
+    let mut newmask: sigset_t;
+    let mut oldmask: sigset_t;
+    //
+    let sig_action: sigaction;
+
+    // Terminal window size
+    let winp = winsize {
+        ws_col: cols as u16, ws_row: rows as u16,
+        ws_xpixel: 0, ws_ypixel: 0
+    };
+
+    // Create a new termios with default flags.
+    // For more info on termios settings:
+    // https://man7.org/linux/man-pages/man3/termios.3.html
     let term = termios {
         c_iflag: ICRNL | IXON | IXANY | IMAXBEL | BRKINT,
         c_oflag: OPOST | ONLCR,
         c_cflag: CREAD | CS8 | HUPCL,
         c_lflag: ICANON | ISIG | IEXTEN | ECHO | ECHOE | ECHOK | ECHOKE | ECHOCTL,
-        c_cc: val,
-        c_ispeed: val,
-        c_ospeed: val
+        c_cc: Default::default(),
+        c_ispeed: Default::default(),
+        c_ospeed: Default::default()
     };
 
+    // Enable utf8 support if requested
     if utf8 { term.c_iflag |= IUTF8; }
 
+    // Set supported terminal characters
     term.c_cc[VEOF] = 4;
-    term.c_cc[VEOL] = -1;
-    term.c_cc[VEOL2] = -1;
+    term.c_cc[VEOL] = 255;
+    term.c_cc[VEOL2] = 255;
     term.c_cc[VERASE] = 0x7f;
     term.c_cc[VWERASE] = 23;
     term.c_cc[VKILL] = 21;
@@ -104,36 +136,87 @@ fn fork<T: Fn(i32,i32) -> Result<()>>(
     term.c_cc[VMIN] = 1;
     term.c_cc[VTIME] = 0;
 
+    // Specific character support for macos
     #[cfg(target_os = "macos")]
     {
         term.c_cc[VDSUSP] = 25;
         term.c_cc[VSTATUS] = 20;
     }
 
-    cfsetispeed(&term, B38400);
-    cfsetospeed(&term, B38400);
+    unsafe {
+        // Set terminal input and output baud rate
+        cfsetispeed(term.borrow_mut(), B38400);
+        cfsetospeed(term.borrow_mut(), B38400);
 
-    let fd = -1;
-    let newmask;
-    let oldmask;
+        // temporarily block all signals
+        // this is needed due to a race condition in openpty
+        // and to avoid running signal handlers in the child
+        // before exec* happened
+        sigfillset(newmask.borrow_mut());
+        pthread_sigmask(SIG_SETMASK, newmask.borrow_mut(), oldmask.borrow_mut());
+    }
 
+    // Forks and then assigns a pointer to the fork file descriptor to master
+    let pid = pty_forkpty(master, term, winp);
 
-    // TODO fill in remaining implementation
-    let pty = unsafe { ptsname(fd)? };
-    return Ok(IUnixProcess {fd, pid, pty});
+    if pid == 0 {
+        // remove all signal handler from child
+        sig_action.sa_sigaction = SIG_DFL;
+        sig_action.sa_flags = 0;
+        unsafe {
+            sigemptyset(sig_action.sa_mask.borrow_mut());
+            for i in 0..NSIG {
+                sigaction(i, &sig_action, null() as *mut nix::libc::sigaction);
+            }
+        }
+    }
+
+    // Reenable signals
+    unsafe { pthread_sigmask(SIG_SETMASK, oldmask.borrow_mut(), null() as *mut u32); }
+
+    match pid {
+        -1 => { return Err(napi::Error::new(napi::Status::GenericFailure, "forkpty(3) failed.".to_string())) },
+        0 => {
+            if !cwd.is_empty() {
+                unsafe {
+                    if chdir(cwd.borrow()).is_err() { panic!("chdir(2) failed."); }
+
+                    if uid != -1 && gid != -1 {
+                        if setgid(gid as u32) == -1 { panic!("setgid(2) failed."); }
+                        if setuid(uid as u32) == -1 { panic!("setuid(2) failed."); }
+                    }
+                    // Allocate a vector the size of the args, with space for a file and null terminator
+                    let argv = Vec::<*const *const i8>::with_capacity(args.len() + 2);
+                    // Set the file as the first argument
+                    argv[0] = CString::new(file)?.as_ptr() as *const *const i8;
+                    // Terminate the argument array with null to designate the end
+                    argv[argv.len()-1] = null();
+                    // Fill the existing arguments into the middle of the array
+                    for i in 0..argv.len() - 2 {
+                        argv[i+1] = CString::new(args[i])?.as_ptr() as *const *const i8;
+                    }
+                    pty_execvpe(file, argv.as_slice()[0], env);
+
+                    panic!("execvp(3) failed.")
+
+                };
+            }
+        },
+        _ => {}
+    };
+
+    let pty = pty_ptsname(master)?;
+    return Ok(IUnixProcess {fd: master, pid, pty});
 
 }
 
 #[napi]
 fn open(cols: u32, rows: u32) -> napi::Result<IUnixOpenProcess> {
-    let ws_col = u16::try_from(cols);
-    let ws_row = u16::try_from(rows);
-    if ws_col.is_err() { return Err(napi::Error::new(napi::Status::InvalidArg, String::from("Failed to convert cols to a u16"))) }
-    if ws_row.is_err() { return Err(napi::Error::new(napi::Status::InvalidArg, String::from("Failed to convert rows to a u16"))) }
     let winp = winsize {
-        ws_col: ws_col.unwrap(), ws_row: ws_row.unwrap(),
+        ws_col: cols as u16, ws_row: rows as u16,
         ws_xpixel: 0, ws_ypixel: 0
     };
+
     let (master, slave) = pty_openpty(winp, None, None)?;
 
     pty_nonblock(master.as_raw_fd())?;
@@ -187,7 +270,12 @@ fn resize(fd: i32, cols: i32, rows: i32) -> Result<()>{
 /// execvpe(3) is not portable.
 /// http://www.gnu.org/software/gnulib/manual/html_node/execvpe.html
 fn pty_execvpe(file: String, argv: *const *const i8, envp: *const *const i8) -> i32 {
-    // TODO implementation here
+    unsafe {
+        let environ: *mut *mut *mut c_char;
+        #[cfg(target_os = "macos")] { environ = _NSGetEnviron(); }
+        #[cfg(not(target_os = "macos"))] { environ = environ; }
+    }
+    return 0;
 }
 
 /// Nonblocking FD
@@ -199,19 +287,16 @@ fn pty_nonblock(fd: RawFd) -> Result<i32> {
     Ok(flags)
 }
 
-/// pty_waitpid
 /// Wait for SIGCHLD to read exit status.
 fn pty_waitpid() {
     // TODO implementation here
 }
 
-/// pty_after_waitpid
 /// Callback after exit status has been read.
 fn pty_after_waitpid() {
     // TODO implementation here
 }
 
-/// pty_after_close
 /// uv_close() callback - free handle data
 fn pty_after_close() {
     // TODO implementation here
@@ -257,11 +342,27 @@ fn pty_openpty(winp: winsize, name: Option<String>, termp: Option<&Termios>) -> 
     return Ok((0,0));
 }
 
-fn pty_forkpty(name: String, termp: Termios, winsize: winsize) -> Result<i32> {
-    // TODO implementation here
-    return Ok(0);
+/// Passes the call to the unsafe function forkpty
+#[cfg(target_os = "macos")]
+fn pty_forkpty(mut master: i32, mut termp: termios, mut winp: winsize) -> i32 {
+    unsafe {
+        forkpty(
+    master.borrow_mut(),
+        *null(),
+        termp.borrow_mut() as *mut nix::libc::termios,
+        winp.borrow_mut() as *mut nix::libc::winsize
+        )
+    }
 }
 
+/// Get's the name of the terminal pointed to by the given file descriptor
+fn pty_ptsname(master: &PtyMaster) -> Result<String> {
+    let name = unsafe { ptsname(master) };
+    match name {
+        Ok(name) => return Ok(name),
+        Err(err) => return Err(napi::Error::new(napi::Status::GenericFailure, "Failed to get slave name".to_string())),
+    }
+}
 
 
 
